a7fa0ae68843d84845aaf04b5ff2e8b87196fba0
Nelalanan <126635098+Nelalanan@users.noreply.github.com>
2024-09-12 09:09:33 +0800

    Adding Nel's favorite quote from valorant

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@
 * 1. DALISAY, Keane Emanuel "To be interesting, be interested"
 * 2. AZARCON, Aaron John "Im a Brat when I'm bumping that"
 * 3. TAGANAHAN, Domenic "The One Piece...The One Piece is real!"
-* 4. ALANAN, Nel Adryan
+* 4. ALANAN, Nel Adryan "If you're not a good shot today, don't worry. There are other ways to be useful."
 *
 */
 
